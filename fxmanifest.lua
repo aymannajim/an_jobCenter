d6aa7b0aaaa781dec0f1c549c110bf1fe88dc5a0
aymannajim <36258300+aymannajim@users.noreply.github.com>
2022-09-27 13:48:54 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,5 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+description 'Job Center / Job Tutorials'
+author 'aymannajim' -- For support: https://discord.gg/f2Nbv9Ebf5
+client_script 'client.lua'
